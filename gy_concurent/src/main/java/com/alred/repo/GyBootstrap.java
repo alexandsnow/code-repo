06f29b389e7f7d72904f1665177fbdf3fe3fa14d
GaoYang <18817393346@163.com>
2018-09-02 20:23:55 +0800

    init project

---
+++ gy_concurent/src/main/java/com/alred/repo/GyBootstrap.java
@@ -0,0 +1,8 @@
+package com.alred.repo;
+
+
+public class GyBootstrap {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ gy_concurent/src/test/java/com/alred/repo/GyBootstrap.java
@@ -0,0 +1,8 @@
+package com.alred.repo;
+
+/**
+ * Unit test for simple GyBootstrap.
+ */
+public class GyBootstrap {
+
+}
